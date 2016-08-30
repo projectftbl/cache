60e48392b0a630d7e02a59bb4842a415a8ad144f
Johnny Hall <johnny@recipher.co.uk>
2016-08-30 11:38:30 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+## @ftbl/cache
+
+Cache helper.
---
+++ test/cache.js
@@ -0,0 +1,12 @@
+var should = require('chai').should()
+  , sut = require('../lib/cache');
+
+describe('cache', function() {
+  
+  before(function() {
+  });
+  
+  it('should', function() {
+  });
+  
+});
